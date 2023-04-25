7c4febfbd6564c8410c8a67bea19cb92b3e3f99b
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-04-25 12:02:20 -0300

    Adiciona um spinner ao botão de submit e implementa a lógica de autenticação com tratamento de erros e exibição do toast em caso de falha

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -11,15 +15,43 @@ export class LoginComponent {
     email: '',
     password: '',
   };
+  isLoading = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private _router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  signIn(e: Event) {
+  handleError(error: HttpErrorResponse) {
+    if (error.status === 401) {
+      this.isLoading = false;
+    }
+  }
+
+  async signIn(e: Event) {
     e.preventDefault();
+    this.isLoading = true;
 
-    this.authService.login({
-      email: this.user.email,
-      password: this.user.password,
-    });
+    this.authService
+      .login({
+        email: this.user.email,
+        password: this.user.password,
+      })
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) {
+            this.isLoading = false;
+            this.toastr.error('Email ou senha incorretos', '', {
+              progressBar: true,
+            });
+          }
+
+          return throwError(() => new Error(error.error.message));
+        })
+      )
+      .subscribe((res) => {
+        this._router.navigate(['/platform/home']);
+      });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,34 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
-
-import { AuthInterceptor } from '../interceptors/auth.interceptor';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(
-    private http: HttpClient,
-    private _router: Router,
-    private cookieService: CookieService
-  ) {}
+  constructor(private http: HttpClient) {}
 
   login(user: { email: string; password: string }) {
-    this.http
-      .post('https://api.trakto.io/auth/signin', JSON.stringify(user), {
+    return this.http.post(
+      'https://api.trakto.io/auth/signin',
+      JSON.stringify(user),
+      {
         headers: {
           'Content-Type': 'application/json',
           Accept: '*/*',
         },
-      })
-      .subscribe((res: any) => {
-        console.log(res);
-        AuthInterceptor.accessToken = res['access_token'];
-        this.cookieService.set('accessToken', res['access_token']);
-
-        this._router.navigate(['/platform/home']);
-      });
+      }
+    );
   }
 }
