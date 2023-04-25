6e8503a168c67c9bed63352d99697fec32c5da93
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-04-25 11:58:42 -0300

    Adiciona o módulo Toastr para exibição de mensagens de notificação na aplicação, juntamente com o módulo de animações do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { MaterialComponent } from './components/material/material.component';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,14 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
     MaterialComponent,
     HeaderComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
