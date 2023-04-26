20d2cf5f1fa4480d044102de1abb115138296375
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-04-25 21:01:00 -0300

    Cria componente Card, que exibe informações de uma apresentação

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+
+interface Slide {
+  id: string;
+  title: string;
+  thumbs: {
+    low: string;
+  };
+  pages: string[];
+}
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent {
+  @Input() slide: Slide = {
+    id: '',
+    title: '',
+    thumbs: {
+      low: '',
+    },
+    pages: [],
+  };
+}
