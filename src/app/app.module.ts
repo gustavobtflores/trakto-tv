87be0620ef900ea4835383c4bb65c5f0fea96abc
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-04-27 17:47:56 -0300

    Adiciona o módulo ReactiveFormsModule e refatora o componente de login para utilizar FormGroup e Validators ao invés de ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { CookieService } from 'ngx-cookie-service';
 
@@ -34,6 +34,7 @@ import { CardComponent } from './components/card/card.component';
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [
     {
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, throwError } from 'rxjs';
@@ -13,53 +14,42 @@ import { StorageService } from 'src/app/services/storage.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  user = {
-    email: '',
-    password: '',
-  };
   isLoading = false;
+  loginForm!: FormGroup;
 
   constructor(
     private authService: AuthService,
     private _router: Router,
     private toastr: ToastrService,
-    private storageService: StorageService
+    private storageService: StorageService,
+    private formBuilder: FormBuilder
   ) {}
 
-  handleError(error: HttpErrorResponse) {
-    if (error.status === 401) {
-      this.isLoading = false;
-    }
+  get formControls() {
+    return this.loginForm.controls;
   }
 
   ngOnInit() {
     if (this.storageService.isLogged()) {
       this._router.navigate(['/platform/home']);
     }
+
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
   async signIn(e: Event) {
     e.preventDefault();
+    if (this.loginForm.invalid) {
+      this.isLoading = false;
+      return;
+    }
+
     this.isLoading = true;
-
-    this.authService
-      .login({
-        email: this.user.email,
-        password: this.user.password,
-      })
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          if (error.status === 401) {
-            this.isLoading = false;
-            this.toastr.error('Email ou senha incorretos', '', {
-              progressBar: true,
-            });
-          }
-
-          return throwError(() => new Error(error.error.message));
-        })
-      )
-      .subscribe((res) => {
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (res) => {
         const user: User = {
           name: res['firstname'],
           avatar: res['logo']['url']['raw']['url'],
@@ -68,6 +58,17 @@ export class LoginComponent {
         this.storageService.setAccessToken(res['access_token']);
         this.storageService.setUser(user);
         this._router.navigate(['/platform/home']);
-      });
+      },
+      error: (err) => {
+        if (err.status === 401) {
+          this.isLoading = false;
+          this.toastr.error('Email ou senha incorretos', '', {
+            progressBar: true,
+          });
+        }
+
+        return throwError(() => new Error(err.error.message));
+      },
+    });
   }
 }
