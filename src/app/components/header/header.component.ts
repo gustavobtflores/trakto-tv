184e9caf96443c5b845e34797649c68d127080c7
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-04-27 00:01:05 -0300

    Adiciona botão para trocar ambiente no header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/interfaces/user';
 import { StorageService } from 'src/app/services/storage.service';
 
@@ -8,10 +9,15 @@ import { StorageService } from 'src/app/services/storage.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private storageService: StorageService) {}
+  constructor(
+    private storageService: StorageService,
+    private route: ActivatedRoute
+  ) {}
 
   currentDate = new Intl.DateTimeFormat('pt-BR').format(new Date());
   user: User | null = this.storageService.getUser();
+  isHomeCurrentRoute =
+    this.route.snapshot.routeConfig?.path === 'platform/home';
 
   @Input() theme: 'dark' | 'light' = 'dark';
 }
