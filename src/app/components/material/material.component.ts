72edd3204084b2d611589d4419ac53e7af9774ee
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-04-27 01:05:19 -0300

    Adiciona setas para navegação no slider e tag de datas da semana

--- src/app/components/material/material.component.ts
+++ src/app/components/material/material.component.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Material } from 'src/app/interfaces/material';
 import { DataService } from 'src/app/services/data.service';
-import Swiper from 'swiper';
+import Swiper, { Navigation } from 'swiper';
 
 @Component({
   selector: 'app-material',
@@ -15,13 +14,20 @@ export class MaterialComponent {
   materialSlides: Material[] = [];
 
   ngOnInit() {
+    Swiper.use([Navigation]);
+
     var swiper = new Swiper('.swiper', {
       slidesPerView: 'auto',
       spaceBetween: 32,
       freeMode: true,
+      navigation: {
+        nextEl: '.content__box-controllers--next',
+        prevEl: '.content__box-controllers--prev',
+      },
     });
 
     this.dataService.getMaterials().subscribe((res) => {
+      console.log(res);
       const items = res.data.map(({ id, pages, thumbs, title }) => {
         return {
           id,
